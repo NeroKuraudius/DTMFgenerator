369488abbad480dc48b74525f40bf2cc61a3c8a3
Nero Hsu <nero.hsu@inspiredgt.local>
2025-04-21 17:50:32 +0800

    feat: set views using handlebars

---
+++ app.js
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const port = process.env.PORT || 3001
+const app = express()
+
+app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main'}))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({extended: true}))
+
+app.listen(port, ()=>{
+    console.log(`App starts listening at port: ${port}`)
+})
