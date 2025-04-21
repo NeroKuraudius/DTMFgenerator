f4ece25c5e8554c4db727b523bf95536310b7fff
Nero Hsu <nero.hsu@inspiredgt.local>
2025-04-21 18:06:53 +0800

    feat: set router get('/') & generator.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
-    return res.render('Welcome to express!')
+    return res.send('Welcome to express!')
+})
+
+router.get('/generator', (req,res)=>{
+    return res.render('generator')
 })
 
 module.exports = router
