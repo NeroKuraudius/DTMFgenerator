0b1512f64bdd0fb24b1253e7d5f6be8cded580de
Nero Hsu <nero.hsu@inspiredgt.local>
2025-04-21 18:21:52 +0800

    feat: add utils/DTMFhandler.js

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,16 @@ router.get('/generator', (req,res)=>{
     return res.render('generator')
 })
 
+
+// 主要邏輯:
+router.post('/generator', async(req,res)=>{
+    const { content } = req.body
+    
+    try{
+        const 
+    }catch(err){
+        console.error('Error occupied on POST: /generator', err)
+    }
+})
+
 module.exports = router
---
+++ utils/DTMFhandler.js
@@ -0,0 +1,21 @@
+const DTMFfreqs = require('../DTMFdata.json')
+
+const tone = require('tonegenerator')
+
+const baseRate = 44100
+const toneDuration = 0.5
+
+function generateDTMFTone(digit) {
+    const [t1, t2] = DTMFfreqs[digit]
+    const lowTone = tone(t1, toneDuration, baseRate)
+    const highTone = tone(t2, toneDuration, baseRate)
+    const mixed = lowTone.map((t, i) => (t + highTone[i]) * 0.5)
+    return mixed;
+}
+
+function generateSilence(duration) {
+    const silence = Math.floor(baseRate * duration)
+    return new Array(silence).fill(0)
+}
+
+module.exports = { generateDTMFTone, generateSilence}
